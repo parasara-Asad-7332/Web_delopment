0bf8d947905bb8fcf37a0ca7dffb32b8dcc2b348
Parasara Mohamad Asad <kingofparasara786@gmail.com>
2026-01-01 05:17:26 +0530

    web

---
+++ JS/ch_4.js
@@ -0,0 +1,23 @@
+// let Number = [50, 60, 20, 10];
+
+// // let name = ["Asad","Zahid","Aj"]
+
+// for (let i = 0; i < Number.length; i++) {
+//     console.log(Number[i]);
+// }
+// let Marks =[85,97,44,37,76,60];
+
+// let sum = 0;
+
+// for(let i = 0; i<Marks.length; i++){
+//     sum += Marks[i];
+// }
+// let Avg=sum/Marks.length;
+
+// console.log(Avg);
+
+let Number = [50,40,20,30,10,25];
+
+for(let i=0; i<Number.length; i++){
+    console.log(Number[i]);
+}
---
+++ JS/ch_5.js
@@ -0,0 +1,52 @@
+//functions
+
+// function student() {
+    //fun defi    
+// }
+
+// function myfun() {
+//     console.log("Hello world");
+// }
+// myfun();
+
+// function myfun(msg,n) {//parameter
+//     console.log(msg,n);
+// }
+// myfun("Hello world",100);//argument
+
+
+// function Sum(a,b) {
+//     s = a+b;
+//     // console.log("sum is: ",ans);
+//     return s;
+// }
+// // Sum(10,10);
+
+// let ans = Sum(10,10);
+// console.log("sum is: ",ans);
+
+//Arrow funstions
+// function Sum(a,b) {
+//     s = a+b;
+//     return s;
+// }
+
+// let ans = Sum(10,10);
+// console.log("sum is: ",ans);
+
+// let ArrowSum = (a,b) =>{
+//     console.log("Sum is: ",a+b);
+// };
+
+// ArrowSum(10,40);
+
+function countvowels(str) {
+    let = count=0;
+    for(const char of str){
+        if(char==="a" || char==="e" || char ==="i"|| char==="o" || char==="u"){
+            console.log(char);
+            count++;
+        }
+    }
+    console.log(count);
+}
