a77ad3cb73db6979eace614c4f29e9f538b02288
Parasara Mohamad Asad <kingofparasara786@gmail.com>
2026-01-09 05:35:28 +0530

    js

--- JS/Events/scrip.js
+++ JS/Events/scrip.js
@@ -1,15 +1,46 @@
-let btn1 = document.querySelector("#btn1");
+// let btn1 = document.querySelector("#btn1");
 
-btn1.onclick = () => {
-   console.log("Hello world");
-   let a = 25;
-   let b = 25;
-   console.log("Sum is: ",(a+b))
-};
+// btn1.onclick = (e) => {
+   //    console.log(e);
+   //    console.log(e.type);
+   //    console.log("Hello world");
+   // let a = 25;
+   //    let b = 25;
+   //    console.log("Sum is: ",(a+b))
+   // };
+   // let btn2 = document.querySelector("#btn2");
+   
+   // btn2.ondblclick= () => {
+      //    console.log("Button click 2 Time");
+      // };
+      
+      // let div = document.querySelector("div");
+      
+      // div.onmouseover = () => {
+         //    console.log("My Name is Asad");
+         // }
+         
+         
+// let btn1 = document.querySelector("#btn1");
 
-let div = document.querySelector("div")
+// btn1.addEventListener("click", () => {
+//    console.log("Button is Click!");
+// })
+// btn1.addEventListener("click", () => {
+//    console.log("My Name is Asad");
+// })
 
-div.onmouseover = () =>
-    { console.log("My Name is Asad");
+let mode = document.querySelector("#mode");
 
-    }
+let cmode = "ligth";
+
+mode.addEventListener("click", () => {
+   if(cmode === "light"){
+      cmode = "drak";
+      document.querySelector("body").style.backgroundColor = "black";
+   }
+   else{
+      cmode = "light";
+      document.querySelector("body").style.backgroundColor = "white";
+   }
+})
